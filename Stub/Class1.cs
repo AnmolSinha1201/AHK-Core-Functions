b66dc6b210f48e87d7e355dbbf77882ef0dccd87
Anmol Sinha <anmolsinha1201@outlook.com>
2018-04-23 11:59:56 +0530

    Added basic test stub dll for testing

---
+++ Stub/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using static AHKCore.Nodes;
+using static AHKCore.IndexedNodesFragment.Variables;
+
+namespace AHKCore
+{
+	public class Functions
+	{
+		public static BaseAHKNode Test()
+		{
+			Console.WriteLine("FUNCTIONS LOADED");
+			return null;
+		}
+
+		public static BaseAHKNode Test2(BaseAHKNode text)
+		{
+			return text;
+		}
+
+		public static BaseAHKNode Test3(BaseAHKNode text)
+		{
+			return new VariableValue() {Value = new SomeClass(text.extraInfo.ToString())};
+		}
+
+		public class Inner
+		{
+			public static BaseAHKNode Test()
+			{
+				Console.WriteLine("INNER CLASS");
+				return null;
+			}
+
+			public static BaseAHKNode Test2(BaseAHKNode num1, BaseAHKNode num2)
+			{
+				return new VariableValue(){Value =  
+				(int)((VariableValue)num1).Value + (int)((VariableValue)num2).Value};
+			}
+		}
+	}
+
+	public class SomeClass
+	{
+		string text;
+		public SomeClass(string text)
+		{
+			this.text = text;
+		}
+
+		public BaseAHKNode GetText()
+		{
+			return new VariableValue() {Value = this.text};
+		}
+	}
+}
