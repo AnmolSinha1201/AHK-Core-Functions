63ba0e647e0bb39d5162b51998b469bbf4d3483e
Anmol Sinha <anmolsinha1201@outlook.com>
2018-07-19 16:45:53 +0530

    Added stub2 to test dll dependency loading

---
+++ Stub2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomeRandomStub
+{
+    public class Functions
+    {
+        public static object CallOtherStub()
+        {
+            return AHKCore.Functions.Test2(123);;
+        }        
+    }
+}
